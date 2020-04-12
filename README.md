689e985e540240522b9a83fbb9ae92b2b0647733
dsjena <36220371+dsjena@users.noreply.github.com>
2020-04-13 00:00:03 +0530

    Initial Commits

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ncv-in
-NCV  Charting
+# Prety Chart Node JS
+
+Interative Chart Library inspired by [jeetiss/covid-charts][data-link]. This code is curated to accomodate some state-lebvel data of various country to use another API [dsjena/ncv-api][data-link]  
+
+
+# acknowledgements
+
+Thanks @jeetiss for the initial code [jeetiss/covid-charts][data-link]
+Special thanks to [tradingview][tradingview] for [supa-dupa charts][tradingview-charts].
+
---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+
+const external = ["date-fns", "lightweight-charts"];
+export default [
+  {
+    input: "api/main.js",
+    output: {
+      format: "cjs",
+      file: "cjs/index.js",
+      sourcemap: false
+    },
+    external
+  },
+  {
+    input: "api/main.js",
+    output: {
+      format: "esm",
+      file: "esm/index.js",
+      sourcemap: false
+    },
+    external
+  },
+  {
+    input: "api/main.js",
+    output: {
+      format: "umd",
+      file: "index.js",
+      name: "createChart",
+      sourcemap: false
+    },
+    plugins: [commonjs(), resolve(), terser()]
+  }
+];
